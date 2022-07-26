66917db13230994f09c8d4bba04739355cc98e72
Justin Chang <justin.p.chang@gmail.com>
2022-07-26 18:32:01 -0400

    Basic home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <h1>Hello world</h1>
+    </div>
+  );
 };
 
 export default Home;
