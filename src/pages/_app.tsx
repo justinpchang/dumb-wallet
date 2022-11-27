0962af3c914c5a20c18be41c265062ba417904a2
Justin Chang <justin.p.chang@gmail.com>
2022-11-27 15:05:21 -0500

    Fix page overflow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,8 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
-        <Component {...pageProps} />
-      </div>
+    <div className="h-screen w-screen flex flex-col justify-start items-center pt-10">
+      <Component {...pageProps} />
     </div>
   );
 }
