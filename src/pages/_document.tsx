8700e5d29e5ef2b85aedec5ccb924566454a0f2f
Justin Chang <justin.p.chang@gmail.com>
2022-11-28 20:53:02 -0500

    Update head meta for PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,13 @@ const Document = () => {
   return (
     <Html>
       <Head>
+        <meta name="application-name" content="Moolah" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Moolah" />
+        <meta name="description" content="Minimalist mooney manager" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" />
